0724c6424018cbbad4c77e30b0328e003b5669a9
Gene Chorba <eugene@clearlyinnovative.com>
2014-11-16 00:22:11 -0500

    stuffff

--- employees.js
+++ employees.js
@@ -26,7 +26,7 @@ http.get(apiURL, function(res) {
   res.on('data', function (chunk) {
    // console.log("\tBegin\n" + chunk + "\t\nEnd")
  
-    var employees1 = eval(chunk);
+   // var employees1 = eval(chunk);
    console.log("\tBegin\n" + employees1 + "\t\nEnd")
   });
 }).on('error', function(e) {
