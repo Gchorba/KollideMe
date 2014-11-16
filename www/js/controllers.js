f3ef463bbe40e81f7744971480f55dd1a78ff15c
Dwight Naylor <dwight.naylor@sbcglobal.net>
2014-11-16 06:27:51 -0500

    make it better

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,9 +11,11 @@ angular.module('directory.controllers', [])
 
         $scope.search = function () {
             $scope.employees = Employees.query({name: $scope.searchKey});
+            $scope.myEvents = MyEvents.query({name: $scope.searchKey});
         }
 
         $scope.employees = Employees.query();
+        $scope.myEvents = MyEvents.query();
     })
 
     .controller('EmployeeDetailCtrl', function($scope, $stateParams, Employees) {
