74c40883fbd8a3e2a2066410b07f1302b1ad67cc
Dwight Naylor <dwight.naylor@sbcglobal.net>
2014-11-16 06:21:25 -0500

    make it better

--- www/js/services.js
+++ www/js/services.js
@@ -2,8 +2,4 @@ angular.module('directory.services', ['ngResource'])
 
     .factory('Employees', function ($resource) {
         return $resource('/events/:employeeId/:data');
-    })
-
-    .factory('MyEvents', function ($resource) {
-        return $resource('/events/:employeeId/:data');
     });
